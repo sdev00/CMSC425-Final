d5fc10d90e5096e98075598dcfb2d78e7be9fa64
Sahil Dev <spdev2000@gmail.com>
2021-07-02 03:59:50 -0400

    started on asteroid generation and player movement

---
+++ Assets/Code/GenerateAsteroids.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateAsteroids : MonoBehaviour
+{
+    public GameObject asteroid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            int x = Random.Range(-200, 200);
+            int y = Random.Range(-200, 200);
+            int z = Random.Range(-200, 200);
+            Vector3 pos = new Vector3(x, y, z);
+            Instantiate(asteroid, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int sensitivity = 100;
+    private int sensitivityAdjustment = 20;
+    //private float playerSpeed = 0;
+    private float playerAngleY = 0;
+    private float playerAngleX = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+
+        playerAngleY += Input.GetAxis("Mouse X") * sensitivity / sensitivityAdjustment;
+        playerAngleX = Mathf.Clamp(playerAngleX - Input.GetAxis("Mouse Y") * sensitivity / sensitivityAdjustment, -60, 60);
+
+        transform.rotation = Quaternion.Euler(0, playerAngleY, 0) * Quaternion.Euler(playerAngleX, 0, 0);
+    }
+}
